809b731ee90ee8ead3db3f57c0ce0e06de471ef0
meirdev <maornet@gmail.com>
2022-12-01 20:24:29 +0200

    import tests

--- tests/test_json_pointer.py
+++ tests/test_json_pointer.py
@@ -37,14 +37,25 @@ def test_get(pointer, expected):
     assert get(DATA, pointer) == expected
 
 
+def test_get_error_invalid_pointer():
+    with pytest.raises(Exception) as excinfo:
+        get(["foo", "bar"], "d")
+
+    assert str(excinfo.value) == "Pointer must start with '/'"
+
+
 def test_get_error_invalid_list_index_zeros_leading():
-    with pytest.raises(Exception):
+    with pytest.raises(Exception) as excinfo:
         get(["foo", "bar"], "/01")
+
+    assert "list indices must be integers or slices, not str" in str(excinfo.value)
 
 
 def test_get_error_invalid_list_index():
-    with pytest.raises(Exception):
-        get(["foo", "bar"], "d")
+    with pytest.raises(Exception) as excinfo:
+        get(["foo", "bar"], "/d")
+
+    assert "list indices must be integers or slices, not str" in str(excinfo.value)
 
 
 def test_get_error_key_of_plain_object():
@@ -53,13 +64,17 @@ def test_get_error_key_of_plain_object():
 
 
 def test_get_error_dict_key_not_found():
-    with pytest.raises(Exception):
+    with pytest.raises(Exception) as excinfo:
         get({"foo": "bar"}, "/bar")
+
+    assert repr(excinfo.value) == "KeyError('bar')"
 
 
 def test_get_error_list_index_not_found():
-    with pytest.raises(Exception):
+    with pytest.raises(Exception) as excinfo:
         get(["foo", "bar"], "/2")
+
+    assert "list index out of range" in str(excinfo.value)
 
 
 def test_set_list_index():
@@ -116,8 +131,10 @@ def test_set_error_dict_new_key_not_last_item():
         }
     }
 
-    with pytest.raises(Exception):
+    with pytest.raises(Exception) as excinfo:
         set(data, "/foo/bar2/4", "qux")
+
+    assert str(excinfo.value) == "Pointer to non-existing key must be the last item"
 
 
 def test_set_list_new_item():
@@ -131,22 +148,28 @@ def test_set_list_new_item():
 def test_set_error_root_pointer():
     data = {"foo": ["bar", "baz"]}
 
-    with pytest.raises(Exception):
+    with pytest.raises(Exception) as excinfo:
         set(data, "", "qux")
+
+    assert str(excinfo.value) == "Set pointer must not be empty"
 
 
 def test_set_error_invalid_path():
     data = {"foo": ["bar", "baz"]}
 
-    with pytest.raises(Exception):
+    with pytest.raises(Exception) as excinfo:
         set(data, "/foo/0/1", "qux")
+
+    assert str(excinfo.value) == "string indices must be integers"
 
 
 def test_set_error_hypen_not_last_item():
     data = {"foo": ["bar", "baz"]}
 
-    with pytest.raises(Exception):
+    with pytest.raises(Exception) as excinfo:
         set(data, "/foo/-/1", "qux")
+
+    assert str(excinfo.value) == "'-' must be the last item in the pointer"
 
 
 def test_json_pointer_get():
